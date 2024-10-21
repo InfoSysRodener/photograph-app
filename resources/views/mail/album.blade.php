f21013db42ae092df1ed33ec06a39cfe58325868
infosysrodener <dajesrodener8@gmail.com>
2024-10-21 12:06:02 +0800

    update mail blade

--- resources/views/mail/album.blade.php
+++ resources/views/mail/album.blade.php
@@ -5,9 +5,10 @@
    </p>
    <p>
       {{-- <a href="http://localhost:5173/photographer/album/{{$user->album_id}}/user/{{$user->id}}/{{$hash}}">View your album </a> --}}
-      <a href="http://photo-demo-app.s3-website-ap-southeast-1.amazonaws.com/photographer/album/{{$user->album_id}}/user/{{$user->id}}/{{$hash}}">View your album </a>
-      
    </p>
+   <a href="http://photo-demo-app.s3-website-ap-southeast-1.amazonaws.com/photographer/album/{{$user->album_id}}/user/{{$user->id}}/{{$hash}}">
+      View your album 
+   </a>
 
 
 {{-- <x-mail::button :url="''">
